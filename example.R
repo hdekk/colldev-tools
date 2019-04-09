d614f2a2256b0819164d45532f028c5dd7dd9b2b
Harrison Dekker <hdekker@uri.edu>
2019-04-09 12:01:15 -0400

    first commit

---
+++ example.R
@@ -0,0 +1,18 @@
+library(fuzzyjoin)
+library(dplyr)
+a <- data.frame(name = c('Ace Co', 'Bayes', 'asd', 'Bcy', 'Baes', 'Bays'),
+                price = c(10, 13, 2, 1, 15, 1))
+b <- data.frame(name = c('Ace Co.', 'Bayes Inc.', 'asdf'),
+                qty = c(9, 99, 10))
+
+
+stringdist_join(a, b, 
+                by = "name",
+                mode = "left",
+                ignore_case = FALSE, 
+                method = "jw", 
+                max_dist = 99, 
+                distance_col = "dist") %>%
+  group_by(name.x) %>%
+  top_n(1, -dist)
+
